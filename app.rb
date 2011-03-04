d41ed893d7f49a7797486bbb8f2b4ee3331f28a2
Meng Fung <meng.fung@gmail.com>
2011-03-04 12:07:54 -0800

    Added Bundler & Sass, plus sass /public/stylesheets/stylesheet => /stylesheet.css

--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
 require 'rubygems'
+
+require 'bundler'
+Bundler.setup
+
 require 'sinatra'
 require 'haml'
+require 'sass'
+
+get '/stylesheet.css' do
+  sass :"stylesheets/stylesheet", :style => :compact
+end
 
 get '/' do
   haml :index
