1e206e6eb4aafd04885b09a2b8ec9db0d008ee5a
Meng Fung <meng.fung@gmail.com>
2011-03-04 13:52:56 -0800

    Added rspec test

---
+++ spec/spec_helper.rb
@@ -0,0 +1,16 @@
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+require 'rubygems'
+require 'sinatra'
+require 'rack/test'
+require 'rspec'
+
+# set test environment
+set :environment, :test
+set :run, false
+set :raise_errors, true
+set :logging, false
+
+#Rspec.configure do |config|
+#  config.before(:each) {}
+#end
